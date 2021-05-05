d1a08857dbbdf369e17f4b1cd13f6b9ce790e6cd
ruixiaozi <admin@ruixiaozi.com>
2021-05-06 00:47:53 +0800

    rename file1

--- src/components/Base/RxzCountdownButton/Index.js
+++
@@ -1,8 +0,0 @@
-import RxzCountDownButton from './src/RxzCountDownButton.vue'
-
-/* istanbul ignore next */
-RxzCountDownButton.install = function(Vue) {
-  Vue.component(RxzCountDownButton.name, RxzCountDownButton);
-};
-
-export default RxzCountDownButton;
