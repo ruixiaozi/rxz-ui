a5ce200824a3aca5dade06b6fa3fee561855ddac
ruixiaozi <admin@ruixiaozi.com>
2021-05-05 23:51:05 +0800

    feat(rxz-ui): 增加css引入方式

--- src/test/main.js
+++ src/test/main.js
@@ -2,11 +2,11 @@
  * 用于测试
  */
 import Vue from 'vue'
-import App from './test/App.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-import rxzUI from "./index"
+import rxzUI from "@/index"
 Vue.use(rxzUI);
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/test/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+  },
+  css: {
+    extract: false,
+  },
+
   lintOnSave: false
 }
