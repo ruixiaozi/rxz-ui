43ed42e6e6a909183428daddf76b62a78f87d9cf
ruixiaozi <admin@ruixiaozi.com>
2022-07-16 01:31:24 +0800

    feat(2.0.0): 添加radio组件

--- typings/components.d.ts
+++ typings/components.d.ts
@@ -1,6 +1,7 @@
 declare module '@vue/runtime-core' {
   // GlobalComponents for Volar
   export interface GlobalComponents {
+    RxzRadio: typeof import('../packages/index')['RxzRadioDeclare'],
     RxzInput: typeof import('../packages/index')['RxzInputDeclare'],
     RxzFormItem: typeof import('../packages/index')['RxzFormItemDeclare'],
     RxzForm: typeof import('../packages/index')['RxzFormDeclare'],
