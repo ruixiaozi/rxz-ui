dd59c4665be42045c98c92d471b9d6a5000abd04
ruixiaozi <admin@ruixiaozi.com>
2022-07-16 01:07:14 +0800

    fix(2.0.0): 格式化lint

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HellowWord/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/HellowWord/HelloWorld.vue';
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
     });
